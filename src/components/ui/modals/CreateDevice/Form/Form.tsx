a2232c6541e7c16cb3aad9c46279dc1f03612d94
yuranius <yuranius.webber@gmail.com>
2022-11-16 15:59:24 +0300

    work styled create device

--- src/components/ui/modals/CreateDevice/Form/Form.tsx
+++ src/components/ui/modals/CreateDevice/Form/Form.tsx
@@ -3,6 +3,7 @@ import styled from './Form.module.scss'
 import {IFormCreateDevice} from "../../../../../types/propsTypes";
 import {observer} from "mobx-react-lite";
 import {uuid} from "../../../../../utils/uuid";
+import cn from "classnames";
 
 interface IInfo {
 	number: string
@@ -39,56 +40,58 @@ const Form:FC<IFormCreateDevice> = observer((props) => {
 	return (
 			<form onSubmit={testFunction} className={styled.form}>
 				<div className={styled.select}>
-					<select name='types' className="focus:ring-blue-500 focus:border-blue-500 border-gray-300 bg-gray-50 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+					<select name='types' className="focus:ring-blue-500 focus:border-blue-500 border-gray-300 bg-gray-300 text-gray-700 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 						<option defaultValue={device.selectedType.name}>Выберите тип</option>
 						{types.map((type) => <option key={type.id}  value={type.name} onClick={() => device.setSelectedType(type)}>{type.name}</option>)}
 					</select>
 				</div>
 				<div className={styled.select}>
-					<select name='barnds' className="focus:ring-blue-500 focus:border-blue-500 border-gray-300 bg-gray-50 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+					<select name='barnds' className="focus:ring-blue-500 focus:border-blue-500 border-gray-300 bg-gray-300 text-gray-700 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 						<option defaultValue={device.selectedBrand.name}>Выберите бренд</option>
 						{brands.map((brand) => <option key={brand.id}  value={brand.name} onClick={() => device.setSelectedBrand(brand)}>{brand.name}</option>)}
 					</select>
 				</div>
 
-				<div className='flex flex-col'>
+				<div className={cn(styled.input, 'text-gray-500 dark:text-gray-100')}>
 					<label/> Введите название устройства
-					<input type='text' name="name" />
+					<input type='text' name="name" className='bg-gray-100 dark:bg-gray-700' />
 				</div>
-				<div className='flex flex-col'>
+				<div className={cn(styled.input, 'text-gray-500 dark:text-gray-100')}>
 					<label/> Введите cтоимость устройства
-					<input type='text' name="price" />
+					<input type='text' name="price" className='bg-gray-100 dark:bg-gray-700' />
 				</div>
-				<div className='flex flex-col'>
+				<div className={cn(styled.input, 'text-gray-500 dark:text-gray-100')}>
 					<label/> Выберите файл
 					<input type='file' name="file" />
 				</div>
 
 				<hr />
 
-				<button onClick={addInfo}>Добавить новое свойство</button>
-				{info.map( i =>
-						<div key={i.number} className='mt-3'>
-							<div>
-								<input
-										value={i.title}
-										placeholder='Введите название характеристики'
-										onChange={(e) => changeInfo('title', e.target.value, i.number)}
-								/>
+				<div className={styled.info}>
+					<button onClick={addInfo} className={styled.button}>Добавить новое свойство</button>
+					{info.map( i =>
+							<div key={i.number} className='mt-3'>
+								<div>
+									<input
+											value={i.title}
+											placeholder='Введите название характеристики'
+											onChange={(e) => changeInfo('title', e.target.value, i.number)}
+									/>
+								</div>
+								<div>
+									<input
+											value={i.description}
+											placeholder='Введите описание характеристики'
+											onChange={(e) => changeInfo('description', e.target.value, i.number)}
+									/>
+								</div>
+								<div>
+									<button className={styled.button} onClick={() => deleteInfo(i.number)}>Удалить</button>
+								</div>
 							</div>
-							<div>
-								<input
-										value={i.description}
-										placeholder='Введите описание характеристики'
-										onChange={(e) => changeInfo('description', e.target.value, i.number)}
-								/>
-							</div>
-							<div>
-								<button onClick={() => deleteInfo(i.number)}>Удалить</button>
-							</div>
-						</div>
-				)}
-				<button>Отправить</button>
+					)}
+				</div>
+				<button className={styled.button}>Отправить</button>
 			</form>
 	);
 });
